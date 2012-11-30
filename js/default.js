9bb4e740944799791897bf4eb0fdc7344c8a1fd3
MattTarquin <lord_tarquin@hotmail.com>
2012-11-30 14:06:24 +0000

    basic structure setup complete.

--- js/default.js
+++ js/default.js
@@ -1,5 +1,5 @@
 (function($){
-    document.write('<scr'+'ipt src="/js/modernizr.min.js"></scr'+'ipt>');
+    document.write('<scr'+'ipt src="/js/libs/recorder.js"></scr'+'ipt>');
     document.write('<scr'+'ipt src="/js/webrtc.js"></scr'+'ipt>');
 
     $(document).ready(function(){
